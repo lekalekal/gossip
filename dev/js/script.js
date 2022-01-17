c87d4df217c782eaf4bc6eef0b56e6fa8a485e0a
unknown <lekalekal@ya.ru>
2022-01-17 15:56:51 +0100

    commit

--- dev/js/script.js
+++ dev/js/script.js
@@ -1,20 +1,20 @@
-// const swiper = new Swiper('.swiper2', {
-//     // Optional parameters
-//     loop: true,
-//     autoplay: {
-//         delay: 2500,
-//     },
-//     effect: 'fade',
-//     fadeEffect: {
-//         crossFade: true
-//     },
-//     // Navigation arrows
-//     navigation: {
-//         nextEl: '.next',
-//         prevEl: '.prev',
-//     },
+const swiper2 = new Swiper('.swiper2', {
+    // Optional parameters
+    loop: true,
+    // autoplay: {
+    //     delay: 2500,
+    // },
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.next2',
+        prevEl: '.prev2',
+    },
     
-// });
+});
 
 
 const swiper = new Swiper('.swiper', {
